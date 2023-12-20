ed121843a070787e96cf322030535c331bd3b2fd
MarioPalaciosLop <plopezmarioalfr@gmail.com>
2023-12-19 19:42:36 -0500

    controller con dto

--- src/main/java/com/palacios/denunciaservice/controller/DenunciaController.java
+++ src/main/java/com/palacios/denunciaservice/controller/DenunciaController.java
@@ -12,69 +12,80 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.palacios.denunciaservice.dto.DenunciaDTO;
 import com.palacios.denunciaservice.entity.Denuncia;
 import com.palacios.denunciaservice.service.DenunciaService;
 
+import com.palacios.denunciaservice.converter.DenunciaConverter;
 
 @RestController
 @RequestMapping("/denuncias/")
 public class DenunciaController {
-    
+
+    @Autowired
+    private DenunciaConverter converter;
+
     @Autowired
     private DenunciaService service;
 
     @GetMapping()
-    public ResponseEntity<List<Denuncia>> findAll(
+    public ResponseEntity<List<DenunciaDTO>> findAll(
             @RequestParam(value = "dni", required = false, defaultValue = "") String dni,
             @RequestParam(value = "offset", required = false, defaultValue = "0") int pageNumber,
             @RequestParam(value = "limit", required = false, defaultValue = "5") int pageSize) {
         Pageable page = PageRequest.of(pageNumber, pageSize);
-        List<Denuncia> empresas;
+        List<Denuncia> grupos;
         if (dni == null) {
-            empresas = service.findAll(page);
+            grupos = service.findAll(page);
         } else {
-            empresas = service.findByDni(dni, page);
+            grupos = service.findByDni(dni, page);
         }
 
-        if (empresas.isEmpty()) {
+        if (grupos.isEmpty()) {
             return ResponseEntity.noContent().build();
         }
-        return ResponseEntity.ok(empresas);
+        List<DenunciaDTO> gruposDTO = converter.fromEntity(grupos);
+        return ResponseEntity.ok(gruposDTO);
     }
 
     @GetMapping(value = "{id}")
-    public ResponseEntity<Denuncia> findById(@PathVariable("id") int id) {
-        Denuncia denuncia = service.findById(id);
-        if (denuncia == null) {
+    public ResponseEntity<DenunciaDTO> findById(@PathVariable("id") int id) {
+        Denuncia grupos = service.findById(id);
+        if (grupos == null) {
             return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok(denuncia);
+        DenunciaDTO gruposDTO = converter.fromEntity(grupos);
+        return ResponseEntity.ok(gruposDTO);
     }
 
     @PostMapping()
-    public ResponseEntity<Denuncia> create(@RequestBody Denuncia denuncia) {
-        Denuncia registro = service.save(denuncia);
-        return ResponseEntity.status(HttpStatus.CREATED).body(registro);
+    public ResponseEntity<DenunciaDTO> save(@RequestBody DenunciaDTO denunciasDTO) {
+        Denuncia registro = service.save(converter.fromDTO(denunciasDTO));
+        DenunciaDTO registroDTO = converter.fromEntity(registro);
+        return ResponseEntity.status(HttpStatus.CREATED).body(registroDTO);
     }
 
-    // @PutMapping(value = "{id}")
-    // public ResponseEntity<Denuncia> update(@PathVariable("id") int id,@RequestBody Denuncia denuncia) {
-    //     Denuncia registro = service.update(converter.fromDTO(gruposDTO));
-    //     if (registro == null) {
-    //         return ResponseEntity.notFound().build();
-    //     }
-    //     GruposProveedorDTO registroDTO = converter.fromEntity(registro);
-    //     return ResponseEntity.ok(registroDTO);
-    // }
+    @PutMapping(value = "{id}")
+    public ResponseEntity<DenunciaDTO> update(@PathVariable("id") int id,
+            @RequestBody DenunciaDTO denunciaDTO) {
+        Denuncia registro = service.update(converter.fromDTO(denunciaDTO));
+        if (registro == null) {
+            return ResponseEntity.notFound().build();
+        }
+        DenunciaDTO registroDTO = converter.fromEntity(registro);
+        return ResponseEntity.ok(registroDTO);
+    }
 
-    @DeleteMapping(value = "{id}")
-    public ResponseEntity<Denuncia> delete(@PathVariable("id") int id) {
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<DenunciaDTO> delete(@PathVariable("id") int id) {
         service.delete(id);
         return ResponseEntity.ok(null);
     }
+
 }
