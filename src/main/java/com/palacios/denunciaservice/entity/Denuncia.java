34af23814ab6c306cb4686994ab7306f2a7f9afb
MarioPalaciosLop <plopezmarioalfr@gmail.com>
2023-12-19 19:43:52 -0500

    unnsed librarys

--- src/main/java/com/palacios/denunciaservice/entity/Denuncia.java
+++ src/main/java/com/palacios/denunciaservice/entity/Denuncia.java
@@ -12,8 +12,6 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
